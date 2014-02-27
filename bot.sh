b46f0d6e47077f60baee414b15a426bad6d89829
Michael Layzell <michael@thelayzells.com>
2014-02-27 12:09:24 -0500

    Add a bot to play the game for you

---
+++ bot.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Plays the game for you
+# Super complicated advanced artificial intelligence
+
+# USAGE: ./bot.sh | ./dancing-doves
+
+while [ 1 ]; do
+	for a in 1 2 3 4; do
+		for i in {1..100}; do
+			echo ""
+			sleep 0.01
+		done
+		echo "b"
+		sleep 0.01
+		echo $a
+		sleep 0.01
+	done
+done
